89605adf6c82457edc661334bf9cb18cd4493e6d
Stefan Pejcic <stefan@pejcic.rs>
2024-05-23 22:18:23 +0200

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-
+# setup service
 cp sentinel.timer /etc/systemd/system/sentinel.timer
-cp sentinel.sh /usr/local/bin/sentinel.sh
+cp sentinel.service /etc/systemd/system/sentinel.service
 
+# copy files
+cp sentinel.sh /usr/local/sentinel/sentinel.sh
+sudo chmod +x /usr/local/sentinel/sentinel.sh
 
+# start service
 sudo systemctl daemon-reload
 sudo systemctl enable sentinel.timer
 sudo systemctl start sentinel.timer
