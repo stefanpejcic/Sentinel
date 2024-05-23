670f7010ca1fe347c1e716ba3af45440b7dd0097
Stefan Pejcic <stefan@pejcic.rs>
2024-05-23 22:06:20 +0200

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,3 +3,8 @@
 
 cp sentinel.timer /etc/systemd/system/sentinel.timer
 cp sentinel.sh /usr/local/bin/sentinel.sh
+
+
+sudo systemctl daemon-reload
+sudo systemctl enable sentinel.timer
+sudo systemctl start sentinel.timer
