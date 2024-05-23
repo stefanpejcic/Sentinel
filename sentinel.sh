d44877f204a067d87e5803578f6142e216e7bcae
Stefan Pejcic <stefan@pejcic.rs>
2024-05-23 22:15:37 +0200

    Update sentinel.sh

--- sentinel.sh
+++ sentinel.sh
@@ -1,1 +1,26 @@
 
+#!/bin/bash
+
+SCRIPT_DIR="/usr/local/sentinel"
+
+case "$1" in
+    on_reboot)
+        $SCRIPT_DIR/on_reboot.sh
+        ;;
+    service_status)
+        $SCRIPT_DIR/service_status.sh
+        ;;
+    resource_usage)
+        $SCRIPT_DIR/resource_usage.sh
+        ;;
+    login_check)
+        $SCRIPT_DIR/login_check.sh
+        ;;
+    dos_protection)
+        $SCRIPT_DIR/dos_protection.sh
+        ;;
+    *)
+        echo "Invalid argument"
+        exit 1
+        ;;
+esac
